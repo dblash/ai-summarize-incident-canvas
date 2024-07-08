9440d8227d69f5022d0bed4926270fd68bc836a8
Jeremiah Peoples <105441105+peoplesj@users.noreply.github.com>
2024-07-08 15:34:22 -0500

    Update ai_incident_summary_function.ts
    update function description

--- functions/ai_incident_summary_function.ts
+++ functions/ai_incident_summary_function.ts
@@ -4,7 +4,7 @@ import { DefineFunction, Schema, SlackFunction } from "deno-slack-sdk/mod.ts";
 export const AIIncidentSummaryFunction = DefineFunction({
   callback_id: "ai_incident_summary",
   title: "generate a summary of an incident",
-  description: "Check internal database for a user's mobile information.",
+  description: "Outputs the ai_incident_summary and ai_incident_canvas_title variables",
   source_file: "functions/ai_incident_summary_function.ts",
   input_parameters: {
     properties: {
