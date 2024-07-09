38ce97f051f7e9f60c42593f21024b8895d4dfcc
dblash <81980970+dblash@users.noreply.github.com>
2024-07-09 13:45:14 -0400

    Update manifest.ts

--- manifest.ts
+++ manifest.ts
@@ -1,4 +1,5 @@
 import { Manifest } from "deno-slack-sdk/mod.ts";
+import { AIIncidentSummaryFunction } from "./functions/ai_incident_summary_function.ts";
 
 /**
  * The app manifest contains the app's configuration. This
@@ -6,15 +7,16 @@ import { Manifest } from "deno-slack-sdk/mod.ts";
  * https://api.slack.com/future/manifest
  */
 export default Manifest({
-  name: "CHANGE-APP-NAME",
+  name: "ai-summarize-function-db",
   description: "Summarize an incident with openAI",
   icon: "assets/default_new_app_icon.png",
-  functions: [],
+  functions: [AIIncidentSummaryFunction],
   workflows: [],
-  outgoingDomains: [],
+  outgoingDomains: ["api.openai.com"],
   botScopes: [
     "commands",
     "chat:write",
     "chat:write.public",
+    "channels:history",
   ],
 });
